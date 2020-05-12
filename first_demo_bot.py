2b347645bc897d769a9648784000745b56a806d5
Edward Rodriguez <edwarderf@gmail.com>
2020-05-11 21:46:32 -0300

    termino de dia

--- first_demo_bot.py
+++ first_demo_bot.py
@@ -13,6 +13,12 @@ eur_usd_data = data_request.json()
 all_candles = eur_usd_data["Time Series FX (5min)"]
 
 #obtiene una lista con todas las KEY del diccionario con las velas
-candles_list = list(all_candles)
+candles_id = list(all_candles)
 
-print(candles_list[1])
+print(candles_id[0])
+print(candles_id[1])
+print(candles_id[2])
+print(candles_id[3])
+print(candles_id[4])
+print(candles_id[5])
+print(candles_id[6])
