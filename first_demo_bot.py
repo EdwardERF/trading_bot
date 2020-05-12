d7667feb9ebf4d09f2cb8c4b0b06438e676c8115
Edward Rodriguez <edwarderf@gmail.com>
2020-05-11 23:20:42 -0300

    Obtencion de velas por ID

--- first_demo_bot.py
+++ first_demo_bot.py
@@ -9,16 +9,18 @@ eur_usd_data = data_request.json()
 
 # last_candle = eur_usd_data["Time Series FX (5min)"]["2020-05-11 20:20:00"]
 
-#esto hace un diccionario con las velas
+# esto hace un diccionario con las velas
 all_candles = eur_usd_data["Time Series FX (5min)"]
 
-#obtiene una lista con todas las KEY del diccionario con las velas
+# obtiene una lista con todas las KEY del diccionario con las velas
 candles_id = list(all_candles)
 
+# de esta manera se accede a una ID de la lista
 print(candles_id[0])
-print(candles_id[1])
-print(candles_id[2])
-print(candles_id[3])
-print(candles_id[4])
-print(candles_id[5])
-print(candles_id[6])
+
+print('linea para separar')
+print('-----------------')
+print('Vela numero 1:')
+
+# seleccion de vela por ID
+print(all_candles[candles_id[0]])
